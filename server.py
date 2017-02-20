0b9ff3d9f17698241416b837f5ff5ed103190f2b
Frances Liu <thehamster@gmail.com>
2017-02-20 14:37:00 -0800

    added notes and amended code for demo pt2

---
+++ server.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/greetings')
+def greeting_generator():
+    return render_template("index.html")
+
+
+@app.route('/greeting')
+def greeting():
+    user = request.args.get("person")
+    return render_template("greeting.html", name=user)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
